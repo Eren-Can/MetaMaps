a90583cdb182cc1a2a9c66acd394acd24e6c51af
Eren-Can <erencan20002014@gmail.com>
2022-06-06 19:09:41 +0300

    Add project files.

---
+++ MVC/Areas/Admin/Models/StepModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Areas.Admin.Models
+{
+    public class StepModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "İsim zorunlu alandır.")]
+        [MaxLength(100, ErrorMessage = "Maximum 100 karakter içerebilir")]
+        [MinLength(2, ErrorMessage = "Minimum 2 karakter içermelidir.")]
+        public string Name { get; set; }
+    }
+}
